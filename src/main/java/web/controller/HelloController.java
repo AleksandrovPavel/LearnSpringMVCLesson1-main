c34182dd55e415e83e014851aa9310589642276a
AleksandrovPavel <pvaleksandrov@gmail.com>
2023-06-05 10:36:24 +0500

    add Tasks/LearnSpringMVCLesson1

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,4 +1,28 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "count", required = false) String count,
+                        Model model) {
+
+        int a;
+        String line = "";
+        if (count == null) {
+            a = 1;
+        } else {
+            a = Integer.parseInt(count);
+        }
+        for (int i = 1; i <= a; i++) {
+            line += " Hello world \n";
+        }
+        model.addAttribute("message", line);
+        return "hello";
+    }
 }
